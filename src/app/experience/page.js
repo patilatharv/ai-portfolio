c6562df2d64b6b9e2bc078cddd3faa0ad9f098eb
Atharv Patil <patilatharv24@gmail.com>
2025-06-01 09:08:35 -0400

    Milestone 1: Setup project and added static pages

--- src/app/experience/page.js
+++ src/app/experience/page.js
@@ -4,6 +4,12 @@ const page = () => {
   return (
     <div>
         <h1>My Experience</h1>
+        <h3>Work Experience</h3>
+        <p>Write your work experience in whatever way you want to write it later</p>
+        <h3>Academic Projects</h3>
+        <p>Write about all academic projetcs</p>
+        <h3>Personal Projects</h3>
+        <p>Write about all personal projects</p>
     </div>
   )
 }
--- src/app/resume/page.js
+++ src/app/resume/page.js
@@ -1,9 +1,33 @@
-import React from 'react'
+'use client';
+import React, { useEffect, useState } from 'react';
 
 const page = () => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    // Only run on client
+    setIsClient(true);
+  }, []);
+
   return (
     <div>
         <h1>My Resume</h1>
+        {isClient ? (
+        <iframe
+          src="/resume.pdf"
+          width="100%"
+          height="800px"
+          style={{ border: 'none' }}
+          title="My Resume"
+        />
+      ) : (
+        <p>
+          Your browser does not support viewing PDFs directly.{' '}
+          <a href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+            Click here to download the resume.
+          </a>
+        </p>
+      )}
     </div>
   )
 }
